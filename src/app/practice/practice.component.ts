bd21456cf00b514abaa6ebd531ecbaedb3d68921
srinigoriparthi <srini.goriparthi@gmail.com>
2022-12-26 17:13:03 -0800

    Applying CSS styles dynamically

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -10,6 +10,11 @@ export class PracticeComponent {
   title:string = "Angular Title";
   special:string = "special";
   pecial:string = "pecial";
+  isdisabled:boolean = false;
+
+  isactive:boolean = true;
+  applyc1:boolean = true;
+
   currentDate:Date =  new Date();
   normalDate:string =  new Date().toLocaleDateString();
   normalTime:string =  new Date().toLocaleTimeString();
@@ -17,5 +22,6 @@ export class PracticeComponent {
   timeId  = setInterval(() => {
     this.normalTime=new Date().toLocaleTimeString();
   },1000)
- 
+  
+
 }
